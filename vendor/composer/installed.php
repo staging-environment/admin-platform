23ef8909f2a462f0ad2a476459991c8b5fc0875e
Bonilla <jarodriguezbonilla@gmail.com>
2026-05-02 18:11:13 +0200

    Ultimos cambios

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'fa686f790e61cc53acefc7de4defb0f9591c3b04',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -407,9 +407,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'fa686f790e61cc53acefc7de4defb0f9591c3b04',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
