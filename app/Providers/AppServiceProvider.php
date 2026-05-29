436a2b423f2d5a453efb061adc4f4902508fad47
Bonilla <jarodriguezbonilla@gmail.com>
2026-05-29 11:41:15 +0200

    Changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,17 @@ class AppServiceProvider extends ServiceProvider
             \Illuminate\Support\Facades\URL::forceRootUrl(config('app.url'));
             $_SERVER['HTTPS'] = 'on';
         }
+
+        if (str_contains(request()->path(), 'upload-file')) {
+            \Illuminate\Support\Facades\Log::info('Signature Debug', [
+                'request_url' => request()->url(),
+                'request_fullUrl' => request()->fullUrl(),
+                'config_app_url' => config('app.url'),
+                'has_valid_signature' => request()->hasValidSignature(),
+                'x_forwarded_proto' => request()->header('X-Forwarded-Proto'),
+                'x_forwarded_port' => request()->header('X-Forwarded-Port'),
+                'x_forwarded_host' => request()->header('X-Forwarded-Host'),
+            ]);
+        }
     }
 }
