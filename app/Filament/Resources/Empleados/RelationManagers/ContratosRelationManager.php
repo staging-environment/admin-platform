88d83617b332f93612d6d5799365a3a67368c04c
Bonilla <jarodriguezbonilla@gmail.com>
2026-06-04 21:26:00 +0200

    Fix Action import namespace in ContratosRelationManager

--- app/Filament/Resources/Empleados/RelationManagers/ContratosRelationManager.php
+++ app/Filament/Resources/Empleados/RelationManagers/ContratosRelationManager.php
@@ -15,7 +15,7 @@ use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Schemas\Schema;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Filament\Tables\Actions\Action;
+use Filament\Actions\Action;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\HtmlString;
 
