483b4334528aad1552cb4538db36939a1b4fe806
Bonilla <jarodriguezbonilla@gmail.com>
2026-06-04 21:38:45 +0200

    Remove seed-perms route

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,6 @@ Route::get('/', function () {
     return redirect('/admin');
 });
 
-Route::get('/seed-perms', function () {
-    \Spatie\Permission\Models\Permission::firstOrCreate(['name' => 'gestion_comentarios_empleados']);
-    \Spatie\Permission\Models\Role::findByName('Admin')->givePermissionTo('gestion_comentarios_empleados');
-    \Spatie\Permission\Models\Role::findByName('Gestor')->givePermissionTo('gestion_comentarios_empleados');
-    return 'Permisos actualizados';
-});
-
 Route::get('/home', function () {
     $gasolineras = [];
     $homeConfig = null;
