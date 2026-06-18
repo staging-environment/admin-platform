746cb7b87e8258d524406d039e24a02c213d3545
Bonilla <jarodriguezbonilla@gmail.com>
2026-06-18 21:01:36 +0200

    fix: remove ShouldQueue interface from scheduled jobs to run synchronously

--- app/Jobs/RefreshMineturJob.php
+++ app/Jobs/RefreshMineturJob.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class RefreshMineturJob implements ShouldQueue
+class RefreshMineturJob
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
