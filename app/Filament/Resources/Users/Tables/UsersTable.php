35dbb2b0cefffe89df973f337e7907264f8fa2fe
Bonilla <jarodriguezbonilla@gmail.com>
2026-06-24 09:42:23 +0200

    feat: use AboveContent layout with 3 columns for filters

--- app/Filament/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -43,7 +43,8 @@ class UsersTable
                     ->preload()
                     ->searchable()
                     ->label('Rol'),
-            ], layout: \Filament\Tables\Enums\FiltersLayout::BeforeContent)
+            ], layout: \Filament\Tables\Enums\FiltersLayout::AboveContent)
+            ->filtersFormColumns(3)
             ->recordActions([
                 EditAction::make(),
             ])
