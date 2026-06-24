952ea4e070e1cf47a81a635d5a9a6eb72e78d725
Bonilla <jarodriguezbonilla@gmail.com>
2026-06-24 09:18:31 +0200

    cleanup:

--- check_photo.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'vendor/autoload.php';
-$app = include 'bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-foreach (\App\Models\Empleado::all() as $e) {
-    echo "ID: {$e->id}, Nombre: {$e->nombre}, Foto: {$e->foto}\n";
-}
