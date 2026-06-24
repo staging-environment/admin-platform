c3c389e4f346653447ce092d056b27e6d25ab1d1
Bonilla <jarodriguezbonilla@gmail.com>
2026-06-24 18:26:02 +0200

    fix: marcar Inicio como activo en /admin o /admin/dashboard

--- run_deploy6.sh
+++ run_deploy6.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /home/bonilla/Projects/admin-platform
-./.ddev/commands/host/deploy "fix: evitar que se remarque Administracion al estar en Inicio"
+./.ddev/commands/host/deploy "fix: marcar Inicio como activo en /admin o /admin/dashboard"
 
 ssh developer@164.68.101.69 'cd ~/Projects/admin-platform && ddev exec php artisan optimize:clear && ddev exec php artisan optimize'
