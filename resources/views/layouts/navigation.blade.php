24283be3c321994a400b0d1360f0be1456ac1c41
Bonilla <jarodriguezbonilla@gmail.com>
2026-06-24 18:24:33 +0200

    fix: evitar que se remarque Administracion al estar en Inicio

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -68,7 +68,7 @@
                         <div class="inline-flex h-full">
                             <x-dropdown align="left" width="60">
                                 <x-slot name="trigger">
-                                    <button class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 focus:outline-none transition duration-150 ease-in-out h-16 {{ (request()->is('admin*') && !request()->is('admin/recursos-humanos*') && !request()->is('admin/file-explorer*') && !request()->is('admin/job-offers*') && !request()->is('admin/job-applications*')) ? 'text-indigo-600 font-bold border-indigo-500' : 'text-gray-500 hover:text-gray-700 hover:border-gray-300 border-transparent' }}">
+                                    <button class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 focus:outline-none transition duration-150 ease-in-out h-16 {{ (request()->is('admin*') && !request()->is('admin') && !request()->is('admin/dashboard') && !request()->is('admin/recursos-humanos*') && !request()->is('admin/file-explorer*') && !request()->is('admin/job-offers*') && !request()->is('admin/job-applications*')) ? 'text-indigo-600 font-bold border-indigo-500' : 'text-gray-500 hover:text-gray-700 hover:border-gray-300 border-transparent' }}">
                                         <span>{{ __('Administración') }}</span>
                                         <svg class="ms-1.5 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                             <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
--- run_deploy5.sh
+++ run_deploy5.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /home/bonilla/Projects/admin-platform
-./.ddev/commands/host/deploy "style: destacar menu activo y asegurar visibilidad de breadcrumbs"
+./.ddev/commands/host/deploy "fix: evitar que se remarque Administracion al estar en Inicio"
 
 ssh developer@164.68.101.69 'cd ~/Projects/admin-platform && ddev exec php artisan optimize:clear && ddev exec php artisan optimize'
