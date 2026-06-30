2862dec4b2529764ced6efdb21ae49a5e0c0a26f
Bonilla <jarodriguezbonilla@gmail.com>
2026-06-30 13:30:21 +0200

    style: aumenta tamano y prominencia de logo en login, favicon de login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" type="image/svg+xml" href="{{ asset('ronda_norte_logo.svg') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -18,7 +19,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <x-application-logo class="w-56 h-auto drop-shadow-md hover:scale-105 transition-transform duration-300" />
                 </a>
             </div>
 
