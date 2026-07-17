915b557105c0fcdff06a473841b26ad2abd89c3b
Bonilla <jarodriguezbonilla@gmail.com>
2026-07-17 08:03:33 +0200

    Corregir importacion de la clase Grid para que provenga de Filament\Schemas\Components\Grid

--- app/Filament/Resources/Empleados/Pages/EditEmpleado.php
+++ app/Filament/Resources/Empleados/Pages/EditEmpleado.php
@@ -10,7 +10,7 @@ use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\FileUpload;
-use Filament\Forms\Components\Grid;
+use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Components\Utilities\Get;
 
 class EditEmpleado extends EditRecord
